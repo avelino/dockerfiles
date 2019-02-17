9783cc07ff77a98e3995bb43ff66fc1c664f4475
Avelino <t@avelino.xxx>
2019-02-17 11:08:34 -0300

    upgrade go packages tools

--- theiaide/install.sh
+++ theiaide/install.sh
@@ -1,37 +1,37 @@
-#Developer tools
-
-apt-get update && apt-get -y \
-    install git sudo python python-dev build-essential vim-nox htop
-
 #LSPs
 
 ##GO
-
-#Required to use go get with git source
-apt-get update && apt-get install -y git
-
 export GO_VERSION=1.11.4
-export GOPATH=/usr/local/go-packages
+export GOPATH=/home/project
 export GO_ROOT=/usr/local/go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:${GOPATH}/bin
 
+mkdir -p /home/theia/.go && \
+    echo '{"toolsGopath":"/home/theia/go"}' > /home/theia/.go/go.json
+
 curl -sS https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u -v github.com/ramya-rao-a/go-outline
 go get -u -v github.com/acroca/go-symbols
-go get -u -v github.com/nsf/gocode
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v github.com/haya14busa/goplay/cmd/goplay
+go get -u -v github.com/josharian/impl
+go get -u -v github.com/tylerb/gotype-live
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/godoc
 go get -u -v github.com/zmb3/gogetdoc
-go get -u -v golang.org/x/lint/golint
-go get -u -v github.com/fatih/gomodifytags
-go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v github.com/golang/lint/golint
 go get -u -v github.com/cweill/gotests/...
-go get -u -v golang.org/x/tools/cmd/guru
-go get -u -v github.com/josharian/impl
-go get -u -v github.com/haya14busa/goplay/cmd/goplay
+go get -u -v github.com/alecthomas/gometalinter
+go get -u -v honnef.co/go/tools/...
+go get -u -v github.com/sourcegraph/go-langserver
+go get -u -v github.com/derekparker/delve/cmd/dlv
 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
 
 #Java
